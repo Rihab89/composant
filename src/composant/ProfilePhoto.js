4d742657dd10487f1b8870ce836d6a1293404da9
Rihab89 <mallek.89@outlook.fr>
2022-06-14 14:46:49 +0100

    final

--- src/composant/ProfilePhoto.js
+++ src/composant/ProfilePhoto.js
@@ -1,9 +1,12 @@
 import React from "react";
+import pic from "../Capture d’écran 2022-04-04 124158.png";
 
 const ProfilePhoto = () => {
   return (
     <div>
-      <img src="https://img.passeportsante.net/1200x675/2021-06-01/i107848-eduquer-un-chaton.jpeg" />
+      {/* <img src="https://img.passeportsante.net/1200x675/2021-06-01/i107848-eduquer-un-chaton.jpeg" /> */}
+      <img src={pic} alt="" />
+      <img src="/logo192.png" alt="" />
     </div>
   );
 };
